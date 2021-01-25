c7db38a0dcee171cddb72a113f80c64dbd80ad7a
Thomas Leplus <thomasleplus@users.noreply.github.com>
2021-01-24 22:53:26 -0800

    Switching to ubuntu keyserver

--- pgp-verify-jar/pgp-verify-jar.sh
+++ pgp-verify-jar/pgp-verify-jar.sh
@@ -2,14 +2,24 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [ -z ${KEYSERVER+x} ]; then
+    KEYSERVER="keyserver.ubuntu.com"
+fi
+
 for artifact in "$@"
 do
-    echo checking ${artifact}...
+    echo Checking ${artifact}
     IFS=':' read -ra coordinates <<< "${artifact}"
     groupId="${coordinates[0]}"
     artifactId="${coordinates[1]}"
     artifactVersion="${coordinates[2]}"
-    curl -s -S -o "${artifactId}-${artifactVersion}.jar" "https://repo1.maven.org/maven2/${groupId}/${artifactId}/${artifactVersion}/${artifactId}-${artifactVersion}.jar"
-    curl -s -S -o "${artifactId}-${artifactVersion}.jar.asc" "https://repo1.maven.org/maven2/${groupId}/${artifactId}/${artifactVersion}/${artifactId}-${artifactVersion}.jar.asc"
-    gpg --auto-key-locate keyserver --keyserver pgp.mit.edu --keyserver-options auto-key-retrieve --verify "${artifactId}-${artifactVersion}.jar.asc" "${artifactId}-${artifactVersion}.jar"
+    artifactUrl="https://repo1.maven.org/maven2/${groupId//\.//}/${artifactId}/${artifactVersion}/${artifactId}-${artifactVersion}.jar"
+    artifactFile="${artifactId}-${artifactVersion}.jar"
+    signatureUrl="${artifactUrl}.asc"
+    signatureFile="${artifactFile}.asc"
+    echo Downloading ${artifactUrl}
+    curl -f -s -S -o "${artifactFile}" "${artifactUrl}"
+    echo Downloading ${signatureUrl}
+    curl -f -s -S -o "${signatureFile}" "${signatureUrl}"
+    gpg --auto-key-locate keyserver --keyserver "${KEYSERVER}" --keyserver-options auto-key-retrieve --verify "${signatureFile}" "${artifactFile}"
 done
