58df1b9e4f135a3e2de988c3d61b0d2a1b9baff2
Thomas Leplus <thomasleplus@users.noreply.github.com>
2021-01-25 10:38:04 -0800

    Replacing OFFLINE_KEYS=1 with VERIFICATION_MODE=offline

--- pgp-verify-jar/pgp-verify-jar.sh
+++ pgp-verify-jar/pgp-verify-jar.sh
@@ -2,25 +2,32 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ -z ${OFFLINE_KEYS+x} ]; then
+if [ -z ${VERIFICATION_MODE+x} ]; then
+    VERIFICATION_MODE='online'
+fi
+
+if [ "${VERIFICATION_MODE}" = 'online' ]; then
+    \echo "Using online verification mode."
     if [ -z ${KEYSERVER+x} ]; then
-	KEYSERVER="keyserver.ubuntu.com"
+	KEYSERVER='keyserver.ubuntu.com'
     fi
     if [ -z ${ONLINE_KEYS+x} ]; then
-	echo No key specified, all keys from server "${KEYSERVER}" can be used.
+	\echo No online key specified, all keys from server "${KEYSERVER}" can be used.
     else
 	for key in ${ONLINE_KEYS//,/ }; do
-	    echo Downloading key "${key}" from server "${KEYSERVER}"
-	    gpg --keyserver "${KEYSERVER}" --recv-keys "${key}"
+	    \echo Downloading key "${key}" from server "${KEYSERVER}"
+	    \gpg --keyserver "${KEYSERVER}" --recv-keys "${key}"
 	done
     fi
 else
-    echo "Switching to offline keys mode."
+    \echo "Using offline verification mode."
+    unset KEYSERVER
+    unset ONLINE_KEYS
 fi
     
 for artifact in "$@"
 do
-    echo Checking ${artifact}
+    \echo Checking "${artifact}"
     IFS=':' read -ra coordinates <<< "${artifact}"
     groupId="${coordinates[0]}"
     artifactId="${coordinates[1]}"
@@ -29,13 +36,13 @@ do
     artifactFile="${artifactId}-${artifactVersion}.jar"
     signatureUrl="${artifactUrl}.asc"
     signatureFile="${artifactFile}.asc"
-    echo Downloading ${artifactUrl}
-    curl -f -s -S -o "${artifactFile}" "${artifactUrl}"
-    echo Downloading ${signatureUrl}
-    curl -f -s -S -o "${signatureFile}" "${signatureUrl}"
-    if [ -z ${OFFLINE_KEYS+x} ] && [ -z ${ONLINE_KEYS+x} ]; then
-	gpg --auto-key-locate keyserver --keyserver "${KEYSERVER}" --keyserver-options auto-key-retrieve --verify "${signatureFile}" "${artifactFile}"
+    \echo Downloading "${artifactUrl}"
+    \curl -f -s -S -o "${artifactFile}" "${artifactUrl}"
+    \echo Downloading "${signatureUrl}"
+    \curl -f -s -S -o "${signatureFile}" "${signatureUrl}"
+    if [ "${VERIFICATION_MODE}" = 'online' ] && [ -z ${ONLINE_KEYS+x} ]; then
+	\gpg --auto-key-locate keyserver --keyserver "${KEYSERVER}" --keyserver-options auto-key-retrieve --verify "${signatureFile}" "${artifactFile}"
     else
-	gpg --verify "${signatureFile}" "${artifactFile}"
+	\gpg --verify "${signatureFile}" "${artifactFile}"
     fi
 done
